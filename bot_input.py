fe1fca03c803ab279bd892e0b3447ddbab9005e2
Branimirius <73712742+Branimirius@users.noreply.github.com>
2022-03-03 19:40:02 +0100

    first two phases

--- bot_input.py
+++ bot_input.py
@@ -1,7 +1,52 @@
+
 from send_DTO import Action, InputAction
 
 
+phase = 0
+
+step = 0
+
+
 def bot_input(dto):
-    
-    return InputAction('C', [Action(x = 0, y = 0, cardid = 6, amount = 1), Action(x = 0, y = 0, cardid = 0, amount = 1)]).toJSON()
-    
+    global phase
+    global step
+
+    if phase == 0 and step == 0:
+        step = step + 1
+        return InputAction('C', [Action(x=0, y=0, cardid=6, amount=1), Action(x=0, y=0, cardid=0, amount=1)]).toJSON()
+
+    if phase == 0 and step == 1:
+        step = step + 1
+        return InputAction('P', [Action(cardid=6, x=0, y=0,)]).toJSON()
+
+    if phase == 0 and step == 2:
+        step = step + 1
+        return InputAction('W', [Action(amount=1, x=0, y=0,)]).toJSON()
+
+    if phase == 0 and step == 3:
+        phase = phase + 1
+        step = 0
+        return InputAction('H', [Action(x=0, y=0,)]).toJSON()
+
+
+    if phase == 1 and step == 0:
+        step = step + 1
+        return InputAction('L', [Action(x=1, y=0)]).toJSON()
+
+
+    if phase == 1 and step == 1:
+        step = step + 1
+        return InputAction('C', [Action(x=0, y=0, cardid=5, amount=2), Action(x=0, y=0, cardid=0, amount=10)]).toJSON()
+
+    if phase == 1 and step == 2:
+        step = step + 1
+        return InputAction('P', [Action(cardid=5, x=0, y=0,), Action(cardid=5, x=1, y=0,)]).toJSON()
+
+    if phase == 1 and step == 3:
+        step = step + 1
+        return InputAction('W', [Action(amount=5, x=0, y=0,), Action(amount=5, x=1, y=0,)]).toJSON()
+
+    if phase == 1 and step == 4:
+        phase = phase + 1
+        step = 0
+        return InputAction('H', [Action(x=0, y=0,), Action(x=1, y=0,)]).toJSON()
